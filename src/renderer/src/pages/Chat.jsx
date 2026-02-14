b36d7615a3a91bcd5eb99f8fcd8d440fcc8700c8
Mazees <madaadha21@gmail.com>
2026-02-14 23:29:39 +0700

    feat: implement error "Gagal dapet info dari internet nih, koneksi atau captcha mungkin bermasalah." if search result empty

--- src/renderer/src/pages/Chat.jsx
+++ src/renderer/src/pages/Chat.jsx
@@ -16,42 +16,53 @@ const Chat = () => {
       ...prev.filter((item) => !item.isSearching),
       { role: 'ai', content: '...', isSummarizing: true }
     ])
-    try {
-      const searchSummary = await getSearchResult(
-        search,
-        result,
-        searchProp.current.userInput,
-        searchProp.current.signal,
-        searchProp.current.chatSession
-      )
+    if (result.length > 0) {
+      try {
+        const searchSummary = await getSearchResult(
+          search,
+          result,
+          searchProp.current.userInput,
+          searchProp.current.signal,
+          searchProp.current.chatSession
+        )
+        setChatData((prev) => [
+          ...prev.filter((item) => !item.isSummarizing),
+          {
+            role: 'ai',
+            content: searchSummary.answer,
+            sources: searchSummary.sources,
+            isMemorySaved: true
+          }
+        ])
+        insertMemory({
+          type: 'fact',
+          key: 'misc',
+          memory: JSON.stringify(searchSummary.answer)
+        })
+      } catch (error) {
+        console.error('Search Technical Error:', error)
+        if (error.name === 'AbortError') {
+          setChatData((prev) => [...prev.filter((item) => !item.isSearching)])
+          setChatData((prev) => prev.slice(0, -1))
+        } else {
+          setChatData((prev) => [
+            ...prev.filter((item) => !item.isSummarizing),
+            {
+              role: 'ai',
+              content:
+                'Gagal dapet info dari internet nih, koneksi atau captcha mungkin bermasalah.'
+            }
+          ])
+        }
+      }
+    } else {
       setChatData((prev) => [
         ...prev.filter((item) => !item.isSummarizing),
         {
           role: 'ai',
-          content: searchSummary.answer,
-          sources: searchSummary.sources,
-          isMemorySaved: true
+          content: 'Gagal dapet info dari internet nih, koneksi atau captcha mungkin bermasalah.'
         }
       ])
-      insertMemory({
-        type: 'fact',
-        key: 'misc',
-        memory: JSON.stringify(searchSummary.answer)
-      })
-    } catch (error) {
-      console.error('Search Technical Error:', error)
-      if (error.name === 'AbortError') {
-        setChatData((prev) => [...prev.filter((item) => !item.isSearching)])
-        setChatData((prev) => prev.slice(0, -1))
-      } else {
-        setChatData((prev) => [
-          ...prev.filter((item) => !item.isSearching),
-          {
-            role: 'ai',
-            content: 'Gagal dapet info dari internet nih, koneksi atau captcha mungkin bermasalah.'
-          }
-        ])
-      }
     }
   }
 
