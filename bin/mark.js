5bc97aac0827405f26ecda6523e49b144f93e0f0
Mazees <madaadha21@gmail.com>
2026-09-26 20:44:32 +0700

    fix: error terminal

--- bin/mark.js
+++ bin/mark.js
@@ -10,6 +10,13 @@ import { stopDaemon } from '../src/server/tools/pc-agent.js'
 import { server } from '../src/server/index.js'
 import { closeUI } from '../src/server/launcher.js'
 
+process.on('uncaughtException', (err) => {
+  console.error('\n\x1b[31m[MARK Fatal Error]:\x1b[0m', err)
+  console.error('\x1b[33mTerminal tidak ditutup otomatis agar pesan error di atas dapat dibaca.\x1b[0m')
+  console.error('Tekan Ctrl+C untuk menutup terminal ini.\n')
+  setInterval(() => {}, 60000)
+})
+
 process.on('SIGINT', () => {
   console.log('\n\x1b[33m● Mematikan MARK Core Engine...\x1b[0m')
   closeUI()
@@ -24,4 +31,7 @@ process.on('SIGINT', () => {
 
 runMonitor().catch((err) => {
   console.error('\x1b[31m[Startup Error]:\x1b[0m', err)
+  console.error('\x1b[33mTerminal tidak ditutup otomatis agar pesan error di atas dapat dibaca.\x1b[0m')
+  console.error('Tekan Ctrl+C untuk menutup terminal ini.\n')
+  setInterval(() => {}, 60000)
 })
